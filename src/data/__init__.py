8d91fec9f96c369688c65950604462ffb43e9cea
julius <kittler.julius@gmail.com>
2020-11-18 21:18:12 +0100

    Add first version of base classes and use full flake8 linting

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,1 @@
-"""The :mod:`src.data` module includes classes and methods for fetching raw data and converting it to a format needed by the models and policies."""
+"""The :mod:`src.data` module includes classes for data processing."""
