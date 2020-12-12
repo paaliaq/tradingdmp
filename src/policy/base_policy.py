c210bfae6bda0bb19e4f6e6279e95a0e2b4432a0
julius <kittler.julius@gmail.com>
2020-12-12 15:29:15 +0100

    Update base_policy.py

--- src/policy/base_policy.py
+++ src/policy/base_policy.py
@@ -1,31 +1,42 @@
-"""Base class for all policy classes."""
+"""Base classes for policies."""
 
 from abc import ABC, abstractmethod
-from typing import Any
-
-import pandas as pd
+from typing import Any, Dict
 
 
 class BasePolicy(ABC):
-    """Base class for policies that return a position."""
+    """Base class for policies."""
 
     @abstractmethod
-    def get_position(self, y: pd.DataFrame, *args: Any, **kwargs: Any) -> int:
+    def get_position(
+        self, input_dict: Dict[str, Dict[str, Any]], *args: Any, **kwargs: Any
+    ) -> Dict[str, Dict[str, Any]]:
         """Method for returning a position.
 
-        This function should fit the model given some training data x and y.
+        This function should take a dict that contains, among others, the predicted
+        price percentage change perc_chg for each ticker symbol that is to be considered
+        for trading. Then, this function should return a dict that contains, among
+        others, the percentage of the depot (cash + value) that is to be allocated to
+        the respective ticker symbols by the trader.
 
         Args:
-            y (pd.DataFrame): Predicted targets in data frame of shape (1, d)
+            input_dict: A dictionary with key-value pairs, where each key is a ticker
+                symbol and each value is another dict. This other dict must contain the
+                key "perc_chg" representing the predicted price percentage change.
+                Example: {"ticker1:" {"perc_chg": 0.01}, "ticker2": {"perc_chg": 0.4},
+                ..., "tickerN": {"perc_chg": -0.25}}
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
 
         Returns:
-            qty (int): Quantity of shares of the particular ticker symbol that
-                    should be owned by the trader from the next time step onwards.
-                    If qty == 0, the trader should sell all shares if it owns any.
-                    If qty > 0, the trader has to buy shares unless it already owns
-                    qty shares of the particular ticker.
+            output_dict: A dictionary with key-value pairs, where each key is a ticker
+            symbol and each value is another dict. This other dict must contain the
+            key "weight", representing the percentage of the depot value (cash + value
+            of all investments) of the trader that is supposed to be allocated to the
+            respective ticker symbol by the trader. One of the keys must be called
+            "cash" and holds the percentage of depot value to be allocated to cash.
+            Example: {"cash: {"weight": 0.125}, "ticker1:" {"weight": 0.1},
+            "ticker2": {"weight": 0.05}, ..., "tickerN": {"weight": 0.0}}
 
         """
         pass
