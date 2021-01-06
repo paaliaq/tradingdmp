3c56b34031e1981fbff94e5b83e89ca3f386d6e5
augusttell <august.andersen@outlook.com>
2021-01-06 14:58:24 +0100

    Fixed some bugs and cleaned up code. Should be fully functional

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,10 +1,10 @@
 """utility function used for DMP."""
-import datetime
+from datetime import datetime
 import pymongo
 import pandas as pd
 
 
-class Rawdata:
+class RawData:
     """Class used for fetching raw data from database."""
 
     def __init__(self, mongodbkey: str):
@@ -12,45 +12,48 @@ class Rawdata:
         self.db = pymongo.MongoClient(mongodbkey)
 
     def bors_data(
-        self,
-        granularity: str,
-        ticker_list: list,
-        dt_start: datetime.datetime = None,
-        dt_end: datetime.datetime = None,
+            self,
+            granularity: str,
+            ticker_list: list,
+            dt_start: datetime = datetime(2000, 1, 1),
+            dt_end: datetime = datetime.today(),
     ) -> pd.DataFrame:
         """Fetches raw data from bors data source based on time and ticker selection."""
         iter_len = len(ticker_list)
 
-        if granularity == "quarterly":
-            collection = self.db["bors-data-quarterly"]
         if granularity == "yearly":
             collection = self.db["bors-data-yearly"]
+            time_key = "report_End_Date"
+        if granularity == "quarterly":
+            collection = self.db["bors-data-quarterly"]
+            time_key = "report_End_Date"
         if granularity == "daily":
             collection = self.db["bors-data-daily"]
+            time_key = "Time"
 
-        if iter_len > 1:  # more than one tickers
-            for x in range(0, iter_len):
+        for x in range(0, iter_len):
+            try:
                 iter_query_result = pd.DataFrame(
-                    collection[ticker_list[x]].find_one(
-                        {"date": {"$gte": dt_start, "$lte": dt_end}}
+                    collection[ticker_list[x]].find(
+                        {time_key: {"$gte": dt_start, "$lte": dt_end}}
                     )
                 )
+            except Exception:
+                print(ticker_list[x], "failed")
+                continue
 
-                iter_query_result.columns = [
-                    str(col) + "_" + str(ticker_list[x])
-                    for col in iter_query_result.columns
-                ]
+            iter_query_result.set_index(time_key, inplace=True)
 
-                if x == 0:
-                    query_result = iter_query_result
-                else:  # Join in to dataframe
-                    query_result = query_result.merge(iter_query_result, on="date")
+            iter_query_result.columns = [
+                str(col) + "_" + str(ticker_list[x])
+                for col in iter_query_result.columns
+            ]
 
-        else:
-            query_result = pd.DataFrame(
-                collection[ticker_list[0]].find_one(
-                    {"date": {"$gte": dt_start, "$lte": dt_end}}
-                )
-            )
+            if x == 0:
+                query_result = iter_query_result
+            else:  # Join in to dataframe
+                query_result = query_result.join(iter_query_result,
+                                                 how='outer',
+                                                 on=time_key)
 
         return query_result
