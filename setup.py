3b1c373848b1700a0c8704a279e567f0bc8904a8
augusttell <august.andersen@outlook.com>
2021-02-21 18:12:57 +0100

    versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="tradingdmp",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.4.0",
     description="This repo defines core classes of our trading applications.",
     author="Julius Kittler, August Andersen",
     license="",
