64ddac877824817428e3850d864d91ff79dfbb8e
augusttell <august.andersen@outlook.com>
2021-04-18 17:19:46 +0200

    updates due to linting

--- src/tradingdmp/data/reg/app_data.py
+++ src/tradingdmp/data/reg/app_data.py
@@ -1,6 +1,7 @@
+"""Application classes for data pipelines that are used in our trading apps."""
+
 import datetime
-from functools import reduce
-from typing import Any, Dict, List, Tuple, Union
+from typing import List
 
 import numpy as np
 import pandas as pd
@@ -20,13 +21,17 @@ class DataIBPocReg(BaseFeatureData):
     You can e.g. select 2 years of yearly report data, 4 quarters and 10 days.
 
     """
+
     def __init__(self, mongodbkey: str):
         """Initializes PrepData instance for getting processed data."""
         self.pdata = PrepData(mongodbkey=mongodbkey)
 
-    def _check_inputs_type(self, ticker_list: List[str],
-                           dt_start: datetime.datetime,
-                           dt_end: datetime.datetime) -> None:  # noqa: C901
+    def _check_inputs_type(
+        self,
+        ticker_list: List[str],
+        dt_start: datetime.datetime,
+        dt_end: datetime.datetime,
+    ) -> None:  # noqa: C901
         """Auxilary function to check that the input arguments have the correct type."""
         if not isinstance(ticker_list, list):
             raise ValueError("ticker_list must be of type list.")
@@ -48,14 +53,15 @@ class DataIBPocReg(BaseFeatureData):
         if contains_inf:
             raise ValueError("df_x contains inf or -inf values.")
 
-    def get_data(self,
-                 ticker_list: List[str],
-                 dt_start: datetime.datetime,
-                 dt_end: datetime.datetime,
-                 history_len_daily: int = 10,
-                 history_len_quarterly: int = 4,
-                 history_len_yearly: int = 2,
-                 **kwargs: Any) -> pd.DataFrame:
+    def get_data(
+        self,
+        ticker_list: List[str],
+        dt_start: datetime.datetime,
+        dt_end: datetime.datetime,
+        history_len_daily: int = 10,
+        history_len_quarterly: int = 4,
+        history_len_yearly: int = 2,
+    ) -> pd.DataFrame:
         """Method for getting data that can be passed to a model.
 
         This function fetches pre-processed data from bors-data, Yahoo and Finviz,
@@ -72,23 +78,28 @@ class DataIBPocReg(BaseFeatureData):
             data: If return_training_dfs is False, the return type is
 
         """
-
         self._check_inputs_type(ticker_list, dt_start, dt_end)
 
-        data_daily = self.pdata.bors_data(ticker_list=ticker_list,
-                                          dt_start=dt_start,
-                                          dt_end=dt_end,
-                                          granularity="daily")
+        data_daily = self.pdata.bors_data(
+            ticker_list=ticker_list,
+            dt_start=dt_start,
+            dt_end=dt_end,
+            granularity="daily",
+        )
 
-        data_quarterly = self.pdata.bors_data(ticker_list=ticker_list,
-                                              dt_start=dt_start,
-                                              dt_end=dt_end,
-                                              granularity="quarterly")
+        data_quarterly = self.pdata.bors_data(
+            ticker_list=ticker_list,
+            dt_start=dt_start,
+            dt_end=dt_end,
+            granularity="quarterly",
+        )
 
-        data_yearly = self.pdata.bors_data(ticker_list=ticker_list,
-                                           dt_start=dt_start,
-                                           dt_end=dt_end,
-                                           granularity="yearly")
+        data_yearly = self.pdata.bors_data(
+            ticker_list=ticker_list,
+            dt_start=dt_start,
+            dt_end=dt_end,
+            granularity="yearly",
+        )
 
         # Process each ticker
         iter_count = 0
@@ -96,95 +107,105 @@ class DataIBPocReg(BaseFeatureData):
 
             daily_iter = data_daily.loc[data_daily["ticker"] == ticker]
 
-            quarterly_iter = data_quarterly.loc[data_quarterly["ticker"] ==
-                                                ticker]
+            quarterly_iter = data_quarterly.loc[data_quarterly["ticker"] == ticker]
 
             yearly_iter = data_yearly.loc[data_yearly["ticker"] == ticker]
 
-            if (daily_iter.shape[0] == 0) | (quarterly_iter.shape[0] == 0) | (
-                    yearly_iter.shape[0] == 0):
+            if (
+                (daily_iter.shape[0] == 0)
+                | (quarterly_iter.shape[0] == 0)
+                | (yearly_iter.shape[0] == 0)
+            ):
                 print(ticker, "misses daily, quarterly or yearly")
                 continue
 
-            daily_iter[daily_iter.columns.difference(["date", "ticker"]) +
-                       "_pct_change"] = daily_iter[
-                           daily_iter.columns.difference(["date", "ticker"
-                                                          ])].pct_change()
+            daily_iter[
+                daily_iter.columns.difference(["date", "ticker"]) + "_pct_change"
+            ] = daily_iter[
+                daily_iter.columns.difference(["date", "ticker"])
+            ].pct_change()
 
             quarterly_iter[
-                quarterly_iter.columns.difference(["date", "ticker"]) +
-                "_pct_change"] = quarterly_iter[
-                    quarterly_iter.columns.difference(["date", "ticker"
-                                                       ])].pct_change()
-            yearly_iter[yearly_iter.columns.difference(["date", "ticker"]) +
-                        "_pct_change"] = yearly_iter[
-                            yearly_iter.columns.difference(["date", "ticker"
-                                                            ])].pct_change()
+                quarterly_iter.columns.difference(["date", "ticker"]) + "_pct_change"
+            ] = quarterly_iter[
+                quarterly_iter.columns.difference(["date", "ticker"])
+            ].pct_change()
+            yearly_iter[
+                yearly_iter.columns.difference(["date", "ticker"]) + "_pct_change"
+            ] = yearly_iter[
+                yearly_iter.columns.difference(["date", "ticker"])
+            ].pct_change()
 
             # Processing daily data
             # Convert to numpy array for simple reshaping
             daily_iter_arr = np.array(daily_iter)
 
-            # Create numpy array with daily data features, shape: (observations, (history_len_daily*features))
-            daily_feature_array = np.array([
-                np.stack(daily_iter_arr)[iter:iter + history_len_daily]
-                for iter in range(0, daily_iter.shape[0] - history_len_daily +
-                                  1)
-            ])
-
-            # Extract target variable, shape: (observations-history_len_daily, )
-            daily_tar = np.array(daily_iter[["Close", "Close_pct_change"]])
-            target_array_reshaped = daily_tar[(
-                history_len_daily):daily_iter.shape[0] + 1]
+            # Create numpy array with daily data features
+            # shape: (observations, (history_len_daily*features))
+            daily_feature_array = np.array(
+                [
+                    np.stack(daily_iter_arr)[iter : iter + history_len_daily]
+                    for iter in range(0, daily_iter.shape[0] - history_len_daily + 1)
+                ]
+            )
 
             # Create report publication variable
-            # What date was the report actually reported not which fiscal period it describes
-            quarterly_iter["report_pub_date"] = quarterly_iter.date + \
-                pd.DateOffset(months=3)
-            yearly_iter["report_pub_date"] = yearly_iter.date + \
-                pd.DateOffset(months=3)
+            # What date was the report actually reported not fiscal period it describes
+            quarterly_iter["report_pub_date"] = quarterly_iter.date + pd.DateOffset(
+                months=3
+            )
+            yearly_iter["report_pub_date"] = yearly_iter.date + pd.DateOffset(months=3)
 
             # Creates the quarterly feature array, shape:
             # (history_len_quarterly, quarter features, observations)
             quarterly_feature_array = self._y_q_df_to_arr(
-                daily_feature_array, quarterly_iter, history_len_quarterly)
+                daily_feature_array, quarterly_iter, history_len_quarterly
+            )
 
             # Creates the yearly feature array, shape:
             # (history_len_yearly, yearly features, observations)
-            yearly_feature_array = self._y_q_df_to_arr(daily_feature_array,
-                                                       yearly_iter,
-                                                       history_len_yearly)
+            yearly_feature_array = self._y_q_df_to_arr(
+                daily_feature_array, yearly_iter, history_len_yearly
+            )
 
             # Create transposed quarterly dataframe for each observation in the ticker
-            d_df_iter = self._array_to_df(daily_feature_array,
-                                          history_len_daily, "daily",
-                                          daily_iter)
-            q_df_iter = self._array_to_df(quarterly_feature_array,
-                                          history_len_quarterly, "quarterly",
-                                          quarterly_iter)
+            d_df_iter = self._array_to_df(
+                daily_feature_array, history_len_daily, "daily", daily_iter
+            )
+            q_df_iter = self._array_to_df(
+                quarterly_feature_array,
+                history_len_quarterly,
+                "quarterly",
+                quarterly_iter,
+            )
 
-            y_df_iter = self._array_to_df(yearly_feature_array,
-                                          history_len_yearly, "yearly",
-                                          yearly_iter)
+            y_df_iter = self._array_to_df(
+                yearly_feature_array, history_len_yearly, "yearly", yearly_iter
+            )
 
-            daily_iter = daily_iter.iloc[history_len_daily:, ]
+            daily_iter = daily_iter.iloc[
+                history_len_daily:,
+            ]
             daily_iter.reset_index(drop=True, inplace=True)
             d_df_iter.reset_index(drop=True, inplace=True)
             q_df_iter.reset_index(drop=True, inplace=True)
             y_df_iter.reset_index(drop=True, inplace=True)
 
-            full_df_iter = pd.concat([
-                d_df_iter, q_df_iter, y_df_iter,
-                daily_iter[["date", "ticker", "Close"]]
-            ],
-                                     axis=1)
+            full_df_iter = pd.concat(
+                [
+                    d_df_iter,
+                    q_df_iter,
+                    y_df_iter,
+                    daily_iter[["date", "ticker", "Close", "Close_pct_change"]],
+                ],
+                axis=1,
+            )
 
             if iter_count == 0:
                 complete_df = full_df_iter
             else:
-                #complete_df2 = full_df_iter
-                complete_df = pd.concat([complete_df, full_df_iter],
-                                        join="inner")
+                # complete_df2 = full_df_iter
+                complete_df = pd.concat([complete_df, full_df_iter], join="inner")
 
             print("Finished processing ticker:", ticker)
             iter_count = +1
@@ -197,37 +218,44 @@ class DataIBPocReg(BaseFeatureData):
 
         return complete_df
 
-    def _array_to_df(self, np_array: np.array, seq_len: int, time_type: str,
-                     df: pd.DataFrame) -> pd.DataFrame:
-        """Auxilary that converts numpy array to a transposed dataframe, where
-        with each row matching a day and column matching num_features*sequence length."""
+    def _array_to_df(
+        self, np_array: np.ndarray, seq_len: int, time_type: str, df: pd.DataFrame
+    ) -> pd.DataFrame:
+        """Auxilary that converts numpy array to a transposed dataframe.
+
+        where each row match a day and column matching num_features*sequence length.
+        """
         if time_type == "daily":
             np_array = np.moveaxis(np_array, [0, 1, 2], [2, 0, 1])
 
         # Go through each row and create a new dataframe
         for x in range(0, np_array.shape[2]):
             temp_df = pd.DataFrame(data=np_array[:, :, x], columns=df.columns)
-            temp_df = temp_df[temp_df.columns.difference(['ticker'])]
+            temp_df = temp_df[temp_df.columns.difference(["ticker"])]
 
             # Rank each date for naming, e.g. yearly EBIT_yearly_1, EBIT_Yearly_2
-            if temp_df["date"].values.dtype == '<M8[ns]':  # If al
-                temp_df["date"] = time_type + "_" + \
-                    temp_df["date"].rank(
-                        ascending=False).astype(int).astype(str)
+            if temp_df["date"].values.dtype == "<M8[ns]":  # If al
+                temp_df["date"] = (
+                    time_type
+                    + "_"
+                    + temp_df["date"].rank(ascending=False).astype(int).astype(str)
+                )
             # If not enough historical data, still create the columns but set to 0
             else:
-                temp_df["date"] = list(reversed(range(1, seq_len +
-                                                      1)))  # if missing data
+                temp_df["date"] = list(
+                    reversed(range(1, seq_len + 1))
+                )  # if missing data
                 temp_df["date"] = time_type + "_" + temp_df["date"].astype(str)
 
             # Transpose so that we have each column being a historical value instead of
             # each row
-            temp_df = temp_df.set_index(['date']).unstack()
-            temp_df.index = [
-                '_'.join(map(str, i)) for i in temp_df.index.tolist()
-            ]
-            temp_df.index = temp_df.index.str.lower().str.replace(
-                "-", "_").str.split(" ").str[0]
+            temp_df = temp_df.set_index(["date"]).unstack()
+            temp_df.index = pd.DataFrame(
+                index=["_".join(map(str, i)) for i in temp_df.index.tolist()]
+            ).index
+            temp_df.index = (
+                temp_df.index.str.lower().str.replace("-", "_").str.split(" ").str[0]
+            )
             temp_df = pd.DataFrame(temp_df).transpose()
 
             # Add to main dataframe
@@ -237,19 +265,22 @@ class DataIBPocReg(BaseFeatureData):
                 df_iter = pd.concat([df_iter, temp_df], axis=0)
         return df_iter
 
-    def _y_q_df_to_arr(self, daily_feature_array: np.array, df: pd.DataFrame,
-                       seq_len: int) -> np.array:
+    def _y_q_df_to_arr(
+        self, daily_feature_array: np.ndarray, df: pd.DataFrame, seq_len: int
+    ) -> np.ndarray:
         """Auxilary function that matches quarterly and yearly dates to daily data.
-        You can only have quarterly/yearly data per day that has actually been reported.
-        Creates the quarterly feature array, shape: (seq_len, num_features, obs)"""
 
+        You can only have quarterly/yearly data per day that has actually been reported.
+        Creates the quarterly feature array, shape: (seq_len, num_features, obs)
+        """
         # For each daily observation in dataset
         iter_list = []
         for x in range(0, daily_feature_array.shape[0]):
             # If daily date is before quarterly report pub date set that observations
             # filled with zeroes/skip
             q_rows_match = df[
-                df["report_pub_date"] <= daily_feature_array[x][-1][0]].index
+                df["report_pub_date"] <= daily_feature_array[x][-1][0]
+            ].index
             if len(q_rows_match) < seq_len:
                 feature_array = np.zeros((seq_len, df.shape[1]))
             # Add the last seq_len quarters of data to feature array
