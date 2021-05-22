66ef11962970ade00e30f7dcd07c1a42f9cd9958
Julius Kittler <kittler.julius@gmail.com>
2021-05-22 13:34:59 +0200

    Add get_data_cached as function to the base classes

--- src/tradingdmp/data/clf/base_data.py
+++ src/tradingdmp/data/clf/base_data.py
@@ -1,6 +1,8 @@
 """Base classes for data pipelines."""
 
 import datetime
+import os
+import pickle
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Tuple, Union
 
@@ -16,28 +18,35 @@ class BaseFeatureData(ABC):
     with timeseries data.
     """
 
-    @abstractmethod
-    def get_data(
+    def get_data_cached(
         self,
         ticker_list: List[str],
         dt_start: datetime.datetime,
         dt_end: datetime.datetime,
-        dt_end_required: bool,
-        return_last_date_only: bool,
-        return_training_dfs: bool,
-        return_date_col: bool,
-        return_ticker_col: bool,
-        bins: List[Any],
-        bin_labels: List[str],
+        dt_end_required: bool = False,
+        return_last_date_only: bool = False,
+        return_training_dfs: bool = False,
+        return_date_col: bool = False,
+        return_ticker_col: bool = False,
+        bins: List[Any] = [-np.inf, -0.03, -0.01, 0.01, 0.03, np.inf],
+        bin_labels: List[str] = ["lg_dec", "sm_dec", "no_chg", "sm_inc", "lg_inc"],
         **kwargs: Any
     ) -> Union[
         Dict[str, Tuple[pd.DataFrame, pd.DataFrame]], Tuple[pd.DataFrame, pd.DataFrame]
     ]:
         """Method for getting data that can be passed to a model.
 
-        This function should fetch raw data, clean this data, conduct feature
+        This function should get raw data, clean this data, conduct feature
         engineering and split the data into predictors x and targets y. This function
         should return feature data.
+        This function loads data from a local pickle file if available (instead of
+        fetching it from mongodb). It fetches data from mongodb and writes it to a
+        local pickle file if such a pickle file is not available.
+        You should only use this function for training when you always fetch the same
+        data without changing the input arguments. If you keep changing inputs, you will
+        prefer to use get_data instead. Important: If input arguments change and a
+        previously written out file with data is available, then this out-dated data
+        file will be loaded (although it was fetched based on older input arguments).
 
         Args:
             ticker_list: A list of ticker symbols for which to get data.
@@ -91,6 +100,110 @@ class BaseFeatureData(ABC):
                 x and y. These data frames contain all data points for all dates and
                 tickers.
         """
+        filepath = "cached_data.pkl"
+
+        # Check if cached data is available
+        cached_data_exists = os.path.exists(filepath)
+
+        # Load cached data if available
+        if cached_data_exists:
+            with open(filepath, "rb") as f:
+                data = pickle.load(f)
+                return data
+        # Get and cach data from mongodb if not available
+        else:
+            data = self.get_data(
+                ticker_list=ticker_list,
+                dt_start=dt_start,
+                dt_end=dt_end,
+                dt_end_required=dt_end_required,
+                return_last_date_only=return_last_date_only,
+                return_training_dfs=return_training_dfs,
+                return_date_col=return_date_col,
+                return_ticker_col=return_ticker_col,
+                bins=bins,
+                bin_labels=bin_labels,
+                **kwargs
+            )
+            with open(filepath, "wb") as f:
+                pickle.dump(data, f)
+            return data
+
+    @abstractmethod
+    def get_data(
+        self,
+        ticker_list: List[str],
+        dt_start: datetime.datetime,
+        dt_end: datetime.datetime,
+        dt_end_required: bool = False,
+        return_last_date_only: bool = False,
+        return_training_dfs: bool = False,
+        return_date_col: bool = False,
+        return_ticker_col: bool = False,
+        bins: List[Any] = [-np.inf, -0.03, -0.01, 0.01, 0.03, np.inf],
+        bin_labels: List[str] = ["lg_dec", "sm_dec", "no_chg", "sm_inc", "lg_inc"],
+        **kwargs: Any
+    ) -> Union[
+        Dict[str, Tuple[pd.DataFrame, pd.DataFrame]], Tuple[pd.DataFrame, pd.DataFrame]
+    ]:
+        """Method for getting data that can be passed to a model.
+
+        This function should fetch raw data, clean this data, conduct feature
+        engineering and split the data into predictors x and targets y. This function
+        should return feature data.
+
+        Args:
+            ticker_list: A list of ticker symbols for which to get data.
+            dt_start: All data after incl. dt_start is fetched.
+            dt_end: All data until incl. dt_end is fetched.
+            dt_end_required: Whether data for dt_end is required for a particular ticker
+                symbol. If dt_end_required is true, the returned data_dict will only
+                contain a key-value pair for a ticker if there is data available for
+                this ticker for the dt_end date. Note: if you set return_training_dfs to
+                True and return_date_col to True, then you won't actually get any data
+                points for the dt_end. This is because the last observation is dropped
+                because it only has NA for y and is therefore not useful for training.
+            return_last_date_only: Whether only data for the most recent available date
+                per ticker should be returned. If this is set to True, then return_y
+                is automatically set to False, i.e. y is never returned (since we do not
+                know the price percentage change from the last available date to the
+                next future date). You should set return_last_date_only to true when
+                making predictions during trading.
+            return_training_dfs: Whether data should be returned for model training or
+                not. You will want to set return_training_dfs to True if you need a
+                dataset for model training, validation and testing. If set to True, the
+                data for all tickers is returned as tuple of data frames: (df_x, df_y).
+                Rows with NA values for y will be dropped (i.e. the very last row for
+                each ticker will be dropped). If set to False, the data for all tickers
+                is returned as dictionary of tuples (df_x, df_y), where each key-value
+                pair corresponds to a particular ticker symbol.
+            return_date_col: Whether or not the date column should be kept in df_x.
+            return_ticker_col: Whether or not the ticker column should be kept in df_x.
+            bins: The bins for converting the original continuous target (e.g. price
+                percentage change) into a discrete target y (with at least 2 levels).
+            bin_labels: The labels given to the levels of the discrete target y, which
+                was created according to the input argument 'bins'
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            data: There are two options. Option A) must be implemented by default.
+                Option B) should be implemented for convenience to get the data in a
+                format that is suitable for model fitting.
+                - Option A): If return_training_dfs is False, the return type is
+                Dict[str, Tuple[pd.DataFrame, pd.DataFrame]]. This is a dictionary that
+                contains a key-value pair for each ticker symbol. The key always
+                represents the ticker symbol. (If there is data for each requested
+                ticker, there will be one key for each element in the input ticker_list.
+                If there is not enough data for a requested ticker, however, then this
+                ticker won't appear in the returned dictionary.) Each value is a tuple
+                of two pandas data frames x and y: x of shape (n, m) and y of shape
+                (n, d), where n is the number of samples, m is the number of features
+                and d is the number of target variables.
+                Option B): If return_training_dfs is False, the return type is
+                Tuple[pd.DataFrame, pd.DataFrame]. This is a tuple of two data frames
+                x and y. These data frames contain all data points for all dates and
+                tickers.
+        """
         pass
 
 
@@ -101,6 +214,72 @@ class BaseTimeData(ABC):
     gives pandas data frames with feature data, whereas BaseTimeData gives numpy arrays
     with timeseries data.
     """
+
+    def get_data_cached(
+        self,
+        ticker_list: List[str],
+        dt_start: datetime.datetime,
+        dt_end: datetime.datetime,
+        *args: Any,
+        **kwargs: Any
+    ) -> Dict[str, Tuple[np.ndarray, np.ndarray]]:
+        """Method for getting data that can be passed to a model.
+
+        This function should fetch raw data, clean this data, conduct feature
+        engineering and split the data into predictors x and targets y. This function
+        should return timeseries data.
+        This function loads data from a local pickle file if available (instead of
+        fetching it from mongodb). It fetches data from mongodb and writes it to a
+        local pickle file if such a pickle file is not available.
+        You should only use this function for training when you always fetch the same
+        data without changing the input arguments. If you keep changing inputs, you will
+        prefer to use get_data instead. Important: If input arguments change and a
+        previously written out file with data is available, then this out-dated data
+        file will be loaded (although it was fetched based on older input arguments).
+
+        Args:
+            ticker_list: A list of ticker symbols for which to get data.
+            dt_start: All data after incl. dt_start is fetched.
+                By default, dt_start is None, which means that data is fetched
+                from the first available datetime onward.
+            dt_end: All data until incl. dt_end is fetched.
+                By default, dt_end is None, which means that data is fetched
+                until the last the available datetime.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            data_dict: a dictionary that contains one key-value pair for each
+                ticker symbol. The key always represents the ticker symbol. There is one
+                key for each element in the input ticker_list. Each value is a tuple of
+                two numpy arrays: x of shape (n, t, m) and y of shape (n, d), where n is
+                the number of samples, t is the number of timesteps per sample, m is the
+                number of features per timestep and sample and d is the number of
+                target variables per sample.
+
+        """
+        filepath = "cached_data.pkl"
+
+        # Check if cached data is available
+        cached_data_exists = os.path.exists(filepath)
+
+        # Load cached data if available
+        if cached_data_exists:
+            with open(filepath, "rb") as f:
+                data = pickle.load(f)
+                return data
+        # Get and cach data from mongodb if not available
+        else:
+            data = self.get_data(
+                ticker_list=ticker_list,
+                dt_start=dt_start,
+                dt_end=dt_end,
+                *args,
+                **kwargs
+            )
+            with open(filepath, "wb") as f:
+                pickle.dump(data, f)
+            return data
 
     @abstractmethod
     def get_data(
