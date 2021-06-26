1873ee2a4004079af6dbffa0a201e7555edb1dca
Julius Kittler <kittler.julius@gmail.com>
2021-06-26 12:21:55 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="tradingdmp",
     packages=find_packages(),
-    version="0.8.1",
+    version="0.8.2",
     description="This repo defines core classes of our trading applications.",
     author="Julius Kittler, August Andersen",
     license="",
